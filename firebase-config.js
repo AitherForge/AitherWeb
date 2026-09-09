491efd919f4a4beb69aa3660a15bc3fe11b7ba57
DJTheBest <djthebest53@gmail.com>
2026-09-09 07:21:43 -0500

    Connect AitherWeb to shared Firebase project

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Shared Aither Firebase project configuration.
+// This is the public Web SDK configuration for Firebase project aither-66da8.
+const config = {
+  apiKey: "AIzaSyApbQ7SIZVTcf-LrRiCrVEX2mNIkuCLVHI",
+  authDomain: "aither-66da8.firebaseapp.com",
+  projectId: "aither-66da8",
+  storageBucket: "aither-66da8.firebasestorage.app",
+  messagingSenderId: "625603034284",
+  appId: "1:625603034284:web:c76edf75c4360500e37309",
+  measurementId: "G-EJ0DS5QN89"
+};
+
+export default config;
